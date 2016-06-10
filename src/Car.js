9bc4b2aee855cb74bb85cb288690d5af8b89a8de
Joe Marzullo <jmarzu13@gmail.com>
2016-06-09 17:41:30 -0700

    Linter is dumb

--- src/Car.js
+++ src/Car.js
@@ -7,7 +7,7 @@ function Car(make, model, year, color, seats, passengers) {
   this.running = false;
   this.owner = 'manufacturer';
   this.previousOwners = [];
-  if (passengers == undefined) {
+  if (passengers === undefined) {
     this.passengers = [];
   } else {
     this.passengers = passengers;
@@ -29,55 +29,51 @@ Car.prototype.off = function() {
   this.running = false;
 };
 
-Car.prototype.start = function(newValue) {
+Car.prototype.start = function() {
   this.running = true;
 };
 
 Car.prototype.driveTo = function(newDestination) {
-  if (this.running == true) {
+  if (this.running === true) {
     console.log('Driving to ' + newDestination);
     return true;
-  } else {
-    return false;
   }
+  return false;
 };
 
 Car.prototype.park = function() {
-  if (this.running == false) {
+  if (this.running === false) {
     console.log('Parked');
     return true;
-  } else {
+  }
+  return false;
+};
+
+Car.prototype.pickUp = function(name) {
+  if (this.running === true && (this.seats > (this.passengers.length + 1))) {
+    this.passengers.push(name);
+    console.log('Driving to pick up ' + name);
+    return true;
+  }
+  return false;
+};
+
+Car.prototype.dropOff = function(name) {
+  for (var i = 0; i < this.passengers.length; i++) {
+    if (this.running === true && this.passengers[i] === name) {
+      this.passengers.splice(i, 1);
+      console.log('Dropping off ' + name);
+      return true;
+    }
     return false;
   }
 };
 
-Car.prototype.pickUp = function(name) {
-  if (this.running == true && (this.seats > (this.passengers.length + 1))) {
-    this.passengers.push(name);
-    console.log('Driving to pick up ' + name);
-    return true;
-  } else {
-    return false;
-  }
-}
-
-Car.prototype.dropOff = function(name) {
-  for (var i = 0; i < this.passengers.length; i++) {
-    if (this.running == true && this.passengers[i] === name) {
-      this.passengers.splice(i, 1);
-      console.log('Dropping off ' + name);
-      return true;
-    } else {
-      return false;
-    }
-  }
-}
-
 Car.prototype.passengerCount = function() {
   return this.passengers.length;
-}
+};
 
-Car('Lenny','Blue','Bellingham');
+Car('Lenny', 'Blue', 'Bellingham');
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
