9a68913882f570979d72f7cfb7cf57fea84b5468
derek_robertson <dwilliamrobertson@gmail.com>
2016-06-10 08:45:27 -0700

    car test

--- src/Car.js
+++ src/Car.js
@@ -1,7 +1,10 @@
-function Car(make, model, year, color, seats) {
+function Car(make, model, year, color, seats, passengers) {
   this.make = make;
   this.model = model;
   this.year = year;
+  this.color = color;
+  this.seats = seats;
+  this.passengers = passengers || [];
   // TODO: add color, seats here
 
   this.running = false;
@@ -10,12 +13,68 @@ function Car(make, model, year, color, seats) {
 }
 
 Car.prototype.sell = function(newOwner) {
+  this.previousOwners.push(this.owner);
+  this.owner = newOwner;
   return newOwner;
 };
 
 Car.prototype.paint = function(newColor) {
+  this.color = newColor;
   return newColor;
 };
+
+Car.prototype.start = function(){
+  this.running = true;
+};
+
+Car.prototype.off = function(){
+  this.running = false;
+};
+
+Car.prototype.driveTo = function(destination){
+  if(this.running){
+    console.log("driving to " + destination);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Car.prototype.park = function(){
+  if(!this.running){
+    console.log("parked!!");
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Car.prototype.pickUp = function(name){
+  if(this.running && this.passengers.length + 1 < this.seats){
+    console.log("driving to pick up " + name);
+    this.passengers.push(name);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Car.prototype.dropOff = function(name){
+  for(var i = 0; i < this.passengers.length; i++){
+    if(this.running && this.passengers[i] === name){
+      console.log("driving to drop off " + name);
+      this.passengers.splice(i, 1);
+      return true;
+    } else {
+      return false;
+    }
+  } 
+};
+
+Car.prototype.passengerCount = function(){
+  var integer = this.passengers.length;
+  return integer;
+}
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
