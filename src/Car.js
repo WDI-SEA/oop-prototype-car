65ad7e89b1e0fff0b301feff8e85a1cde9d2f439
Daniel Rasmussen <dan.r.rasmussen@gmail.com>
2016-06-09 17:48:29 -0700

    finished linting

--- src/Car.js
+++ src/Car.js
@@ -16,15 +16,11 @@ function Car(make, model, year, color, seats, passengers) {
 //   }
 }
 
-
-
-
 Car.prototype.sell = function(newOwner) {
   this.previousOwners.push(this.owner);
   this.owner = newOwner;
   return newOwner;
 };
-
 
 Car.prototype.paint = function(newColor) {
   this.color = newColor;
@@ -37,62 +33,48 @@ module.exports = Car;
 
 Car.prototype.off = function() {
   this.running = false;
-}
+};
 
 Car.prototype.start = function() {
   this.running = true;
-}
-
-
-
+};
 
 Car.prototype.driveTo = function(destination) {
   if (this.running === true) {
-    console.log("Driving to " + destination);
+    console.log('Driving to ' + destination);
     return true;
-  } else {
-    return false;
   }
-}
+  return false;
+};
 
 Car.prototype.park = function() {
   if (this.running === false) {
     console.log('parked!!');
     return true;
-  } else {
-    return false;
   }
-}
+  return false;
+};
 
 Car.prototype.pickUp = function(friend) {
-
-  if (this.running == true && (this.passengers.length + 1 < this.seats)) {
-  console.log("Driving to pick up " + friend);
-  this.passengers.push(friend);
-  return true;
-  } else {
-    return false;
+  if (this.running === true && (this.passengers.length + 1 < this.seats)) {
+    console.log('Driving to pick up ' + friend);
+    this.passengers.push(friend);
+    return true;
   }
-}
+  return false;
+};
 
 Car.prototype.dropOff = function(name) {
-  for (i = 0; i < this.passengers.length; i++) {
-    if (this.running == true && this.passengers[i] == name) {
+  for (var i = 0; i < this.passengers.length; i++) {
+    if (this.running === true && this.passengers[i] === name) {
       this.passengers.splice(i, 1);
       console.log('Driving to drop off ' + name);
       return true;
-    } else {
-      return false;
     }
   }
-}
+  return false;
+};
 
 Car.prototype.passengerCount = function() {
   return this.passengers.length;
-}
-
-
-
-
-
-
+};
