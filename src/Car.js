a355044d12264eaeaa73874022c337971cd42bf6
Joe Marzullo <jmarzu13@gmail.com>
2016-06-09 17:34:04 -0700

    YEEEEAAAAAH BOOOOOYYYYYYY

--- src/Car.js
+++ src/Car.js
@@ -1,22 +1,85 @@
-function Car(make, model, year, color, seats) {
+function Car(make, model, year, color, seats, passengers) {
   this.make = make;
   this.model = model;
   this.year = year;
-  // TODO: add color, seats here
-
+  this.color = color;
+  this.seats = seats;
   this.running = false;
   this.owner = 'manufacturer';
   this.previousOwners = [];
+  if (passengers == undefined) {
+    this.passengers = [];
+  } else {
+    this.passengers = passengers;
+  }
 }
 
 Car.prototype.sell = function(newOwner) {
+  this.previousOwners.push(this.owner);
+  this.owner = newOwner;
   return newOwner;
 };
 
 Car.prototype.paint = function(newColor) {
+  this.color = newColor;
   return newColor;
 };
+
+Car.prototype.off = function() {
+  this.running = false;
+};
+
+Car.prototype.start = function(newValue) {
+  this.running = true;
+};
+
+Car.prototype.driveTo = function(newDestination) {
+  if (this.running == true) {
+    console.log('Driving to ' + newDestination);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Car.prototype.park = function() {
+  if (this.running == false) {
+    console.log('Parked');
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Car.prototype.pickUp = function(name) {
+  if (this.running == true && (this.seats > (this.passengers.length + 1))) {
+    this.passengers.push(name);
+    console.log('Driving to pick up ' + name);
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Car.prototype.dropOff = function(name) {
+  for (var i = 0; i < this.passengers.length; i++) {
+    if (this.running == true && this.passengers[i] === name) {
+      this.passengers.splice(i, 1);
+      console.log('Dropping off ' + name);
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+Car.prototype.passengerCount = function() {
+  return this.passengers.length;
+}
+
+Car('Lenny','Blue','Bellingham');
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
 module.exports = Car;
+
