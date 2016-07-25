b7dd2ecdcf359a4b41eef3f1d0a12f88a19bc263
shway250 <jackson_duhon@yahoo.com>
2016-07-25 16:52:22 -0700

    Passed all the tests

--- src/Car.js
+++ src/Car.js
@@ -1,22 +1,99 @@
-function Car(make, model, year, color, seats) {
+function Car(make, model, year, color, seats, passengers) {
   this.make = make;
   this.model = model;
   this.year = year;
   // TODO: add color, seats here
+  this.color = color;
+  this.seats = seats;
 
   this.running = false;
   this.owner = 'manufacturer';
   this.previousOwners = [];
+
+  this.passengers = [];
+  if(!passengers){
+  	this.passengers = [];
+  }else{
+  	this.passengers = passengers;
+  }
 }
 
 Car.prototype.sell = function(newOwner) {
+  this.previousOwners.push(this.owner);
+  this.owner = newOwner;
   return newOwner;
 };
 
 Car.prototype.paint = function(newColor) {
+  this.color = newColor;
   return newColor;
 };
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
+Car.prototype.start =function(){
+	this.running = true;
+}
+Car.prototype.off = function(){
+	this.running = false;
+}
+Car.prototype.driveTo = function(destination){
+	if(this.running){
+		console.log("driving to " + destination);
+		return true
+	}else{
+		return false;
+	}
+}
+Car.prototype.park = function(){
+	if(!this.running){
+		console.log("parked!!!");
+		return true;
+	}else{
+		return false;
+	}
+}
+
+///////Part 3
+
+Car.prototype.pickUp = function(name){
+	if(this.running && (this.passengers.length +1) < this.seats){
+		console.log("driving to pick up " + name);
+		this.passengers.push(name);
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+Car.prototype.dropOff = function(name){
+	if(this.running && this.passengers.indexOf(name) != -1 ){
+		this.passengers.splice(this.passengers.indexOf(name), 1);
+		console.log("driving to drop off " + name);
+		return true;
+	}else{
+		return false;
+	}
+}
+
+Car.prototype.passengerCount = function(){
+	return this.passengers.length;
+}
+
 module.exports = Car;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
