165d3659bca25068e8c06112d48ea3bb276a8a05
ChrisKenyon79 <ckenyon79@gmail.com>
2016-07-26 08:49:05 -0700

    imcomplete phase 2 errors

--- src/Car.js
+++ src/Car.js
@@ -2,11 +2,13 @@ function Car(make, model, year, color, seats) {
   this.make = make;
   this.model = model;
   this.year = year;
-  // TODO: add color, seats here
+  this.color = color; 
+  this.seats = seats;
 
   this.running = false;
   this.owner = 'manufacturer';
   this.previousOwners = [];
+  this.passengers = [];
 }
 
 Car.prototype.sell = function(newOwner) {
@@ -16,6 +18,36 @@ Car.prototype.sell = function(newOwner) {
 Car.prototype.paint = function(newColor) {
   return newColor;
 };
+
+
+
+Car.start = function() {
+	Car.running = true;
+};
+
+Car.off = function() {
+	Car.running = false;
+};
+
+Car.driveTo = function(destination) {
+	if this.running ===true {
+		console.log("Driving to" + destination);
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+Car.park = function() {
+	if this.running ===false {
+		console.log("parked!!")
+		return true;
+	}
+	else {
+		return false;
+	}
+}
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
