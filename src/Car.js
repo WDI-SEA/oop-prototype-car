236c963aeb485da920523531f1e121938a9fd4ef
kendradalley <kendracdalley@gmail.com>
2016-07-25 19:53:02 -0700

    oop prototype deliverable KD

--- src/Car.js
+++ src/Car.js
@@ -1,21 +1,86 @@
-function Car(make, model, year, color, seats) {
-  this.make = make;
-  this.model = model;
-  this.year = year;
-  // TODO: add color, seats here
+function Car(make, model, year, color, seats, passengers) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.seats = seats;
 
-  this.running = false;
-  this.owner = 'manufacturer';
-  this.previousOwners = [];
+    //initialize value to constructor
+    this.previousOwners = [];
+    this.owner = 'manufacturer';
+    this.running = false;
+    //Should be optional and default to an empty array if not specified. If passengers is empty the else portion is fulfilled
+    if (passengers) {
+    	this.passengers = passengers; // if condition 
+    } else {
+    	this.passengers = []; //else 
+	}
+
 }
 
 Car.prototype.sell = function(newOwner) {
-  return newOwner;
-};
+    this.previousOwners.push(this.owner);
+    this.owner = newOwner;
+
+    return newOwner;
+}
 
 Car.prototype.paint = function(newColor) {
-  return newColor;
+        this.color = "Blue";
+        this.color = newColor
+
+        return newColor;
+    }
+    //Phase II
+
+Car.prototype.start = function() {
+    this.running = true;
+}
+
+Car.prototype.off = function() {
+    this.running = false;
+}
+
+Car.prototype.driveTo = function() {
+    if (this.running) {
+        console.log("driving to <destination>");
+        return true;
+    }
+    return false;
+}
+
+Car.prototype.park = function() {
+    if (!this.running) {
+        console.log("parked!!");
+        return true;
+    }
+    return false;
+}
+
+//Phase III
+Car.prototype.pickUp = function(name) {
+	if(this.running && this.passengers.length + 1 < this.seats ){ //add 1 to length b/c car doesn't have a 0 position
+	this.passengers.push(name);
+	console.log("driving to pick up <name>");
+	return true
+}
+return false
+}
+
+Car.prototype.dropOff = function(name) {
+  var passenger = this.passengers.indexOf(name);
+  if (passenger > -1 && this.running) {
+    this.passengers.splice(passenger, 1);
+    return true;
+  }
+  return false;
 };
+
+Car.prototype.passengerCount = function(totalPassengers) {
+var totalPassengers = this.passengers.length;
+return totalPassengers;
+}
+
 
 // export the Car function for use in node //
 // this is required for the test.js to load this //
