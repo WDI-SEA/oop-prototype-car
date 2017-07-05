5da5e7a91f44535516ee039b5fd37d3487d4998c
Prima Aulia Gusta <primaulia@gmail.com>
2017-07-05 18:01:23 +0800

    setup phase 1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 // Phase I
-var Car = require('./carFn')
 
-console.log(Car)
+/*
+* You should make you completed
+* each task step by step. Once you're done with one `Step`
+* be sure to comment the previous console.log
+* so you don't have tornado of console.logs
+*/
+
+// // Step 0 - require / import the carFn file
+
+// // Step 1 - Checking all properties
+// console.log the `Car` object, does it returns all the properties requested?
+
+// // Step 2 - Run `Car.sell()` function
+// `Car` object should be able to run `sell` function,
+// and updates its existing object
+
+// // Step 3 - Run `Car.paint()` function
+// `Car` object should be able to run `paint` function,
+// and updates its existing object
